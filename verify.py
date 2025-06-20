99ebe8db7646e083e7769276566477ee6fb9885e
Araceli Lasso <lassoaraceli@gmail.com>
2025-06-20 16:54:11 -0300

    mejorando prints de testeo

--- verify.py
+++ verify.py
@@ -1,26 +1,33 @@
 from models.army import Army
 
 if __name__ == "__main__":
-    # Crea dos ejércitos
+    #! Crea dos ejércitos
     army1 = Army("chinese")
     army2 = Army("english")
 
-    print("Fuerza inicial de chinese:", army1.get_total_strength())
-    print("Fuerza inicial de english:", army2.get_total_strength())
+    print("\n🔍 Number of units before battle:")
+    print("Chinese:", len(army1.units))  # debería ser 29
+    print("English:", len(army2.units))  # debería ser 30
 
-    # Simula una batalla
-    # army1.attack(army2)
+    print("china initial strength:", army1.get_total_strength())
+    print("english initial strength:", army2.get_total_strength())
+    # Deberia resultar en:
+    # Fuerza inicial de chinese: 300
+    # Fuerza inicial de english: 350
 
-    # print("\n🏁 Después de la batalla:")
-    # print("Fuerza de chinese:", army1.get_total_strength())
-    # print("Fuerza de english:", army2.get_total_strength())
-    # print("Oro de chinese:", army1.gold)
-    # print("Oro de english:", army2.gold)
+    #! Simula una batalla
+    army1.attack(army2)
 
-    # print("\n📜 Historial de batallas:")
-    # print("Chinese:", army1.battle_history)
-    # print("English:", army2.battle_history)
+    print("\n🏁 After the battle:")
+    print("chinese strength:", army1.get_total_strength())
+    print("english strength:", army2.get_total_strength())
+    print("china gold:", army1.gold)
+    print("english gold:", army2.gold)
 
-    # print("\n🔎 Cantidad de unidades:")
-    # print("Chinese:", len(army1.units))
-    # print("English:", len(army2.units))
+    print("\n📜 Battle history:")
+    print("Chinese:", army1.battle_history)
+    print("English:", army2.battle_history)
+
+    print("\n🔎 Number of units after battle:")
+    print("Chinese:", len(army1.units))
+    print("English:", len(army2.units))
