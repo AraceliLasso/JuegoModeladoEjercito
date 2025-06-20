b056e47e5d5b81b1c83d8b5f02c7163cdd258eba
Araceli Lasso <lassoaraceli@gmail.com>
2025-06-20 12:01:25 -0300

    basic modularization

---
+++ data/civili_zations.py
@@ -0,0 +1,5 @@
+INITIAL_UNITS = {
+    "Chinos": {"pikeman": 2, "archer": 25, "knight": 2},
+    "Ingleses": {"pikeman": 10, "archer": 10, "knight": 10},
+    "Bizantinos": {"pikeman": 5, "archer": 8, "knight": 15}
+}
